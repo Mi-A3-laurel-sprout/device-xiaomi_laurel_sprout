45dbd040620f2f6a2cccdcab3bc7ffed3263b3b9
Magicxavi <rolfpeterkesche@gmail.com>
2022-06-25 13:15:25 +0300

    laurel_sprout: drop telephony packages
    We switched to QTI

--- device.mk
+++ device.mk
@@ -466,18 +466,6 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Shim
 PRODUCT_PACKAGES += \
     libims_shim
