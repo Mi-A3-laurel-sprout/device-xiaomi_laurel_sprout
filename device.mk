d3127a70bdd4f1216f8635275d6d0a1c6c80be67
Magicxavi <rolfpeterkesche@gmail.com>
2022-06-26 16:24:34 +0300

    laurel_sprout: drop seccomp policy

--- device.mk
+++ device.mk
@@ -470,11 +470,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libims_shim
 
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.multihal \
