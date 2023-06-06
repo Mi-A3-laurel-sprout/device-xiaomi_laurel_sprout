63153dd12a6c614e60bcc9ee72e1f2a071614de9
Magicxavi <rolfpeterkesche@gmail.com>
2023-06-06 22:55:03 +0300

    laurel_sprout: fingerprint: kang fingerprint service
    * from - https://github.com/LineageOS/android_hardware_xiaomi
    Rename: xiaomi->laurel_sprout.

--- device.mk
+++ device.mk
@@ -183,8 +183,9 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.laurel_sprout
-
+    android.hardware.biometrics.fingerprint@2.3-service.laurel_sprout \
+    android.hardware.biometrics.fingerprint@2.3.vendor \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
