e1ec13d8a4174e5fe6554c88403ed4b6a66b998a
PowerX-NOT <pugalg100@gmail.com>
2023-06-24 17:45:42 +0530

    laurel_sprout: Inherit MiuiCamera

--- device.mk
+++ device.mk
@@ -382,6 +382,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+# MiuiCamera
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
