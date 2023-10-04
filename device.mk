dbfd374d35fe1b8de71705602d5bc323103b5167
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-04 20:36:50 +0530

    laurel_sprout: Switch to Wifi service AIDL
    Change-Id: I5a2864b4159b7815eff4dd2f0de02a3dabbacfa5
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- device.mk
+++ device.mk
@@ -522,7 +522,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwifi-hal-qcom \
     libwifi-hal-ctrl \
