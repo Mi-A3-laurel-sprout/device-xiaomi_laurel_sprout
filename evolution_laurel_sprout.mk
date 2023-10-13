1b470c250c73477cb383444c20263c1e17849d00
Magicxavi <rolfpeterkesche@gmail.com>
2023-10-13 18:30:52 +0300

    laurel_sprout: updatable apexes

--- evolution_laurel_sprout.mk
+++ evolution_laurel_sprout.mk
@@ -22,6 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common EvolutionX stuff
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit from laurel_sprout device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
