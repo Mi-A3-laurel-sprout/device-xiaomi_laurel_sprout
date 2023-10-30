7b6a830e9aea041678360e87866bf62949fd90b1
Michael Bestas <mkbestas@lineageos.org>
2023-10-30 17:38:30 +0200

    laurel_sprout: Enforce vintf kernel requirements
    The build system enables this for shipping API level >=29
    but we also want it to pass the vintf runtime checks.
    
    Change-Id: Ie2147893a1551b736880913334a3c134a17ca91c

--- device.mk
+++ device.mk
@@ -368,6 +368,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Kernel
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_trinket
