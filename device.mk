01aa40d21b5c7d5f3b4644840f26cab74bd2247f
me-cafebabe <me.cafebabe@gmail.com>
2024-06-30 20:40:34 +0300

    laurel_sprout: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992
    [cyberknight777]:
    - Move to RRO and set config_HardwareVideoEncode to false.
    - Set config_HighQualityVideoDefault to true.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -79,6 +79,7 @@ PRODUCT_PACKAGES += \
     XiaomiFrameworks \
     XiaomiSystemUI \
     DisplayFeaturesLAUREL \
+    DeviceAsWebcamLAUREL \
     UdfpsSystemUI \
     RCMSystemUI \
     RCMFramework
