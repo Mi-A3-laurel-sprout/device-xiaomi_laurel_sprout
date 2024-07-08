22ffffe1b16debc5b857214bc7842c1daa50088d
PowerX-NOT <pugalg100@gmail.com>
2024-07-08 10:28:31 +0530

    fix build

--- fingerprint/UdfpsExtension.cpp
+++ fingerprint/UdfpsExtension.cpp
@@ -12,6 +12,8 @@
 #include <drm/sde_drm.h>
 #endif
 
+#define FOD_PRESSED_LAYER_ZORDER 0x20000000u
+
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
     if (touched) {
         z |= FOD_PRESSED_LAYER_ZORDER;
