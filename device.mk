af5decb329f58087d0bf7491ccdfaab927599689
Kyuofox <kyuofox@gmail.com>
2024-09-13 20:12:58 +0200

    laurel_sprout: replace libwvhidl with v34 libcrypto
    On Android 15 Release 1, it will cause the problem of "Cannot find symbol "CBS_init"", This issue occurs on older devices using "android.hardware.drm@1.3-service.widevine", Because of vndk on vendor, see ref[1], "CBS_init" becomes a missing symbol, We can't revert changes, that would cause more things to not work, So replace it with libcrypto from vndk 34, to fix this issue.
    
    [1]: https://android.googlesource.com/platform/system/core/+/ad8cf52c4c2b60250d52f1b0b90648fdf407dff4

--- device.mk
+++ device.mk
@@ -556,7 +556,8 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so \
+    prebuilts/vndk/v34/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v34.so
 
 
 PRODUCT_PRODUCT_VNDK_VERSION := current
