95930737cb6f302b42687c32a356f048036047fb
me-cafebabe <me-cafebabe@users.noreply.github.com>
2024-11-05 18:30:26 +0000

    laurel_sprout: Switch to AIDL USB Gadget HAL

--- device.mk
+++ device.mk
@@ -543,7 +543,7 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/usb/hal/usb_compositions.conf:$(TARGET_COPY_OUT_VENDOR)/etc/usb_compositions.conf
