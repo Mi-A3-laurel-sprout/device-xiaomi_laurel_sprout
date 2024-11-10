dd947f7547d498a1f1d6bd0f04893ac25f71a358
PowerX-NOT <pugalg100@gmail.com>
2024-11-10 18:28:48 +0530

    Revert "laurel_sprout: move to OSS camera hal again"
    This reverts commit a6ca641b191923691270b7a439ff53b82be127d8.

--- device.mk
+++ device.mk
@@ -171,10 +171,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     android.hidl.memory.block@1.0.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor \
     libstdc++.vendor \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
