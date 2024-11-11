01f4d44b75ef80876d15945f1e08a4f0109f0526
PowerX-NOT <pugalg100@gmail.com>
2024-11-11 12:17:18 +0530

    fix build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -235,3 +235,5 @@ include vendor/xiaomi/laurel_sprout/BoardConfigVendor.mk
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+BUILD_BROKEN_PLUGIN_VALIDATION := soong-xiaomi-trinket-plugins
+ALLOW_MISSING_DEPENDENCIES := true
