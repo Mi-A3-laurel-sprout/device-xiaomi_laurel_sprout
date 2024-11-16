aleeeee1/device_xiaomi_laurel_sprout

laurel_sprout: replace libwvhidl with v34 libcrypto
On Android 15 Release 1, it will cause the problem of "Cannot find symbol "CBS_init"", This issue occurs on older devices using "android.hardware.drm@1.3-service.widevine", Because of vndk on vendor, see ref[1], "CBS_init" becomes a missing symbol, We can't revert changes, that would cause more things to not work, So replace it with libcrypto from vndk 34, to fix this issue.

[1]: https://android.googlesource.com/platform/system/core/+/ad8cf52c4c2b60250d52f1b0b90648fdf407dff4

Change-Id: Iea1b77bf2e21c26afa10d180c2676d446af2cafd
Signed-off-by: Kyuofox <kyuofox@gmail.com>
Signed-off-by: tanvirr007 <md.tanvirhasannijum@gmail.com>

device.mk
@@ -162,6 +162,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
+    prebuilts/vndk/v34/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v34.so \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
 
 # FM

Mi-A3-laurel-sprout/device-xiaomi_laurel_sprout

device.mk
@@ -439,7 +439,8 @@ PRODUCT_PACKAGES += \
 BOARD_SHIPPING_API_LEVEL := 30
 
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so \
+    prebuilts/vndk/v34/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v34.so
 
 # Wifi
 PRODUCT_COPY_FILES += \