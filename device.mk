ecec0999566728d7fc84be7f0a9d651821f3d982
PowerX-NOT <pugalg100@gmail.com>
2025-01-26 02:50:55 +0530

    laurel_sprout: Revert DeviceAsWebcam-related changes
    Revert "laurel_sprout: grab usb_compositions.conf from source"
    
    This reverts commit fabcd03ca59a09a4936d328555ff3cdc4e3eda38.
    
    Revert "laurel_sprout: Set TARGET_USES_YCRCB_VENUS_CAMERA_ENCODE"
    
    This reverts commit e8dd9cd102a316ffdf0b64335f23884125b2b4a6.
    
    Revert "laurel_sprout: update init.qcom.usb from Aospa"
    
    This reverts commit 31d8be38418e36f6c5ee69b63c131aadcbda633f.
    
    Revert "laurel_sprout: Migrate to QTI USB Gadget 1.2 HIDL"
    
    This reverts commit 202df36a9e72f1db2fd1361596d2357ef4147868.
    
    Revert "laurel_sprout: Switch to android.hardware.usb@1.3-service.dual_role_usb"
    
    This reverts commit 00a063de9195cbb869c4c6ba42580d0d49e8cfc6.
    
    Revert "laurel_sprout: Move usage of USB MTP/PTP functionality with USB F_FS driver"
    
    This reverts commit e4f18c954a3ff7f328ccd75c6a50fd11e8b739cb.
    
    Revert "laurel_sprout: Setup DeviceAsWebcam"
    
    This reverts commit b6859fa5a4f7d05e8854a02ba3a5907ed8dbb03a.

--- device.mk
+++ device.mk
@@ -79,7 +79,6 @@ PRODUCT_PACKAGES += \
     XiaomiFrameworks \
     XiaomiSystemUI \
     DisplayFeaturesLAUREL \
-    DeviceAsWebcamLAUREL \
     UdfpsSystemUI \
     RCMSystemUI \
     RCMFramework
@@ -541,11 +540,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget-service.qti
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/usb/hal/usb_compositions.conf:$(TARGET_COPY_OUT_VENDOR)/etc/usb_compositions.conf
+    android.hardware.usb@1.0-service
 
 # USB Debugging
 PRODUCT_PROPERTY_OVERRIDES += \
